472ab5476e6fb51bbb2e49275d20d7746ed60d13
maicol <maicol.matteoli@gmail.com>
2021-09-21 16:41:34 +0200

    first commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item" v-for="(link,index) in menu" :key="index">
+            <router-link :to="{name: link.routeName}" class="nav-link">
+              {{link.label}}
+            </router-link>
+            <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name:"Header",
+  data() {
+    return {
+      menu: [
+        {
+          label:'Home',
+          routeName:'home'
+        },
+        {
+          label:'About',
+          routeName:'about'
+        },
+        {
+          label:'Contatti',
+          routeName:'contact'
+        },
+        {
+          label:'I miei Post',
+          routeName:'post'
+        }
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/router.js
@@ -0,0 +1,44 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Post from './pages/Post';
+import SinglePost from './pages/SinglePost';
+const router = new VueRouter({
+  mode: "history",
+
+  routes:[
+    {
+      path: '/',
+      name: "home",
+      component: Home //assegna questa route al componente Home.vue
+    },
+    {
+      path: '/chi-siamo',
+      name: "about",
+      component: About
+    },
+    {
+      path: '/contatti',
+      name:"contact",
+      component: Contact
+    },
+    {
+      path:'/posts',
+      name:'post',
+      component: Post
+    },
+    {
+      path:'/posts:slug',
+      name:'post-detail',
+      component: SinglePost
+    }
+
+  ]
+});
+
+export default router;
